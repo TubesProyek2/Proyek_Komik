07b8de8ebaf6f5af6f9c45b647a9e1f24c3378e3
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2018-11-04 01:08:13 +0700

    menu

---
+++ Assets/Script/btnMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnMenu : MonoBehaviour
+{
+    public void LoadPanel(string pName)
+    { }
+
+    public void Exit()
+    { Application.Quit(); }
+}
