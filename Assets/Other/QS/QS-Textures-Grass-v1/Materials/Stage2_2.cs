0cdff2f26c0ce1092baf69337cce589005fac210
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2018-11-07 09:16:57 +0700

    supo

---
+++ Assets/Other/QS/QS-Textures-Grass-v1/Materials/Stage2_2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2_2 : MonoBehaviour
+{    
+    // Update is called once per frame
+    void OnTriggerEnter()
+    { Application.LoadLevel("Stage2_3"); }
+}
