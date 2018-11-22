caf3c837a741c29b0ca2b2c5766122d7c8c0f406
triska <triskaintania8@gmail.com>
2018-11-22 19:10:03 +0700

    script

---
+++ Assets/Images/Button/ExitMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void PlayGame () {
+        Application.LoadLevel(1);
+	}
+	
+	// Update is called once per frame
+	void Exit () {
+        Application.Quit();
+	}
+}
