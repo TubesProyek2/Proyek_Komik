913dbea62086def0dd68a41983c45a010efcd3b8
Riza Awwalul Baqy <37800718+awangbaqy@users.noreply.github.com>
2018-12-15 00:50:50 +0700

    build

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -6,30 +6,30 @@ public class CameraControl : MonoBehaviour
 {
     private const float Y_ANGLE_MIN = 0.0f;
     private const float Y_ANGLE_MAX = 360.0f;
+    private float currentY = 0.0f;
 
-    public Transform lookAt;
-    public Transform camTransform;
-    public float distance = 10.0f;
 
     public float currentX = -90.0f;
-    private float currentY = 0.0f;
+    public float distance = 10.0f;
     public float sensitivityX = 5.0f;
     public float sensitivityY = 5.0f;
+    public Transform lookAt;
+    public Transform camTransform;
 
     private void Start()
-    {
-        camTransform = transform;
-    }
+    { camTransform = transform; }
 
     private void Update()
     {
         //Set Cursor to not be visible
         Cursor.visible = false;
 
+        Cursor.lockState = CursorLockMode.Confined; // keep confined in the game window
+        Cursor.lockState = CursorLockMode.Locked;   // keep confined to center of screen
+        Cursor.lockState = CursorLockMode.None;     // set to default default
+
         currentX += Input.GetAxis("Mouse X");
         currentY += Input.GetAxis("Mouse Y");
-
-        //currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
     }
 
     private void LateUpdate()
